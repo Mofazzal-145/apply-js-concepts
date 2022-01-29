675d32d3d6451218c57aac4a50ea81ee4c0c19e0
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-01-30 02:52:10 +0600

    factorial-function file added

---
+++ factorial-function.js
@@ -0,0 +1,21 @@
+// let factorial = 1;
+// for (i = 1; i <= 5; i++){
+//     factorial = factorial * i;
+// }
+// console.log(factorial);
+
+function getFactorial(number){
+    let factorial = 1;
+    for(let i = 1; i <= number; i++){
+        factorial = factorial * i;
+    }
+    return factorial;
+}
+
+var firstFactorial = getFactorial(7);
+console.log('Factorial of 7 is',firstFactorial);
+
+var secondFactorial = getFactorial(11);
+console.log("Factorial of 11 is",secondFactorial);
+
+
